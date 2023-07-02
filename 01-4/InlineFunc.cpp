90115da56c624fb6db72ed80ae0696a11b97d653
Imsyp <tkddud386@gmail.com>
2023-07-02 16:40:12 +0900

    InluneFunc

---
+++ 01-4/InlineFunc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+inline int SQUARE(int x)
+{
+    return x*x;
+}
+
+int main(void)
+{
+    std::cout<<SQUARE(3)<<std::endl;
+    std::cout<<SQUARE(5)<<std::endl;
+
+    return 0;
+}
