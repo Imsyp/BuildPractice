2be4f4e21699d079689d18d4dee965b0d708e6fc
Imsyp <tkddud386@gmail.com>
2023-07-06 16:47:05 +0900

    2023.07.06

---
+++ 02-2/CallByRef.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void Swap(int* a, int* b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int main(void)
+{
+    int x = 10;
+    int y = 20;
+
+    cout<<x<<" "<<y<<endl;
+    Swap(&x,&y);
+    cout<<x<<" "<<y<<endl;
+
+    return 0;
+}
