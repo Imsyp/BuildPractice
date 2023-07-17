3f072fced6d9a109f96184fefbf8b048cd65deac
Imsyp <tkddud386@gmail.com>
2023-07-17 19:39:59 +0900

    20230717

--- CPP_Prog_new/06-3/CComStatic.cpp
+++ CPP_Prog_new/06-3/CComStatic.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void Counter()
 {
-    static int cnt;
+    static int cnt; // static 변수는 전역변수와 마찬가지로 초기화하지 않을 시에 0으로 초기화된다.
     cnt++;
     cout<<"Current cnt : "<<cnt<<endl;
 }
