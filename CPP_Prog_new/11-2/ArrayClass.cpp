0697bed61925f4c13388c9e2dbf529e1b37c952b
Imsyp <tkddud386@gmail.com>
2023-07-26 15:56:05 +0900

    20230726

--- CPP_Prog_new/11-2/ArrayClass.cpp
+++ CPP_Prog_new/11-2/ArrayClass.cpp
@@ -12,7 +12,7 @@ public:
     {
         arr = new int[len];
     }
-    int& operator[] (int idx)
+    int& operator[] (int idx)   //배열 요소의 참조값을 반환하여 연속 접근 가능하게 만듦
     {
         if(idx<0 || idx>=arrlen)
         {
@@ -32,7 +32,7 @@ int main(void)
     BoundCheckIntArray arr(5);
     for(int i = 0; i<5; i++)
         arr[i] = (i+1)*11;
-    for (int i = 0; i<6; i++)
+    for (int i = 0; i<6; i++)   //반복의 범위를 1 넘치게 설정
         cout<<arr[i]<<endl;
 
     return 0;
